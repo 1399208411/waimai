a150696f9b85763acb34252239a0123ad2257b0d
“1399208411 <1399208411@qq.com>
2018-06-08 19:33:57 +0800

    李佳远 20150607 第2次提交

--- src/components/rating/Rating.vue
+++ src/components/rating/Rating.vue
@@ -1,17 +1,37 @@
 <!--  -->
 <template>
   <div class="rating" id="rating">
-    评论
+    <div class="ratings-content"
+         v-infinite-scroll="loadMore"
+         infinite-scroll-distance="10">
+      <div class="star-wrapper">
+        <star :score="2.5"></star>
+      </div>
+      <ul>
+        <li v-for="(item,index) in list" :key="index">
+          hello
+        </li>
+      </ul>
+      评论
+    </div>
   </div>
 </template>
 
 <script>
+  import Star from '../star/Star.vue'
 	export default {
 		data() {
-			return {};
+			return {
+			  list:[],
+			  loading:false,
+        allLoaded:false,
+        wrapperHeight:0
+      };
 		},
 
-		components: {},
+		components: {
+		  Star
+    },
 
 		computed: {},
 
@@ -21,7 +41,18 @@
 		mounted() {
 		},
 
-		methods: {}
+		methods: {
+      loadMore() {
+        this.loading = true;
+        setTimeout(() => {
+          let last = this.list[this.list.length - 1];
+          for (let i = 1; i <= 30; i++) {
+            this.list.push(last + i);
+          }
+          this.loading = false;
+        }, 2500);
+      }
+    }
 	}
 </script>
 
